f6eed9149aaec63e29ff3d00814c073302977497
Jesse <jesseluisdiaz@gmail.com>
2014-09-23 01:55:59 -0500

    db tweak

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 var pg = require('pg');
 
-var conString = "postgres://xucaccygtojehx:-0iTWi-fqnlB2JDC1vyYKct-ho@ec2-107-21-226-77.compute-1.amazonaws.com:5432/d2v320pb6k9spn";
+var conString = "pg://xucaccygtojehx:-0iTWi-fqnlB2JDC1vyYKct-ho@ec2-107-21-226-77.compute-1.amazonaws.com:5432/d2v320pb6k9spn";
 
 exports.subscribe = function (name, email) {
   client.query("INSERT INTO subscriber (name, email) values($1, $2)", [name.toString(), email.toString()]);
