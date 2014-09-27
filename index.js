d1d37ab770a660e7f24ecbf39cc432a4b4afe700
Jesse <jesseluisdiaz@gmail.com>
2014-09-26 22:46:51 -0500

    updates

--- index.js
+++ index.js
@@ -42,20 +42,21 @@ function error404(request, response) {
 
 // utils
 function parseBody(request, callback) {
-    var body = '';
-    request.on('data', function(chunk) {
-        body += chunk;
-    });
-    request.on('end', function() {
-        callback(qs.parse(body));
-    });
+  var body = '';
+  request.on('data', function(chunk) {
+    body += chunk;
+  });
+  request.on('end', function() {
+    console.log('about to parse body');
+    callback(qs.parse(body));
+  });
 }
 
 // This function writes to the database then renders a thank you message
 function addNewPost(request, response) {
   var postsHTML = fs.readFileSync('views/posts.html');
   response.writeHead(200, {
-    'content-type': 'text/html; charset=utf-8'
+    'content-type': 'text/html'
   });
   parseBody(request, function(body) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
@@ -63,7 +64,17 @@ function addNewPost(request, response) {
         return console.error('error fetching client from pool', err);
       }
       client.query('CREATE TABLE IF NOT EXISTS subscribers (name varchar(64), email varchar(64))', function(err, result) {
-        client.query("INSERT INTO subscribers (name, email) values($1, $2)", [body.name, body.email]);
+        if(err){
+          console.error('error setting up table');
+        }
+        console.log('table successfully primed');
+        client.query("INSERT INTO subscribers (name, email) values($1, $2)", [body.name, body.email], function(err, result) {
+          if(err) {
+            console.error('error adding user');
+          }
+          console.log('user info added successfully');
+        });;
+
         done();
       });
     });
